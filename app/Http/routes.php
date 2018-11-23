536f4fdb7357bc0c59a1e85b04f98938f12c441a
MohannedM <mohanned.mohammed@msa.edu.eg>
2018-11-23 12:58:18 +0200

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,5 +16,10 @@ Route::get('/', function () {
 });
 
 Route::auth();
+Route::get('/admin', function() {
+    return view('admin.index');
+});
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('/admin/users', 'AdminUsersController');
